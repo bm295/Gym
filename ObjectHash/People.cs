8aac942a07baa4829ad3c8eeef515cee0dea4628
bm295 <baominh.nguyen.295@gmail.com>
2019-07-13 23:00:37 +0700

    init project

---
+++ ObjectHash/People.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectHash
+{
+    class People : IHash
+    {
+        public long? Id { get; set; }                                   /*unique identifier, avoid it to use in hash calculation*/
+        public string Name { get; set; }
+        public bool? IsActive { get; set; }
+        public DateTime? CreatedDateTime { get; set; }
+
+        public string HashString()
+        {
+            var value = new HashHelper().HashString(Name, IsActive, CreatedDateTime);    /*Add more not constant properties as needed*/
+            return value;
+        }
+    }
+
+    [Serializable]
+    class PeopleModel
+    {
+        public long? Id { get; set; }
+        public string Name { get; set; }
+        public bool? IsActive { get; set; }
+        public DateTime? CreatedDateTime { get; set; }
+    }
+}
---
+++ ObjectHash/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ObjectHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*constant: e6fb7af54c39f39507c28a86ad98a1fd*/
+            string name = "Dipon Roy";
+            string value = new HashHelper().HashString(name);
+            Console.WriteLine(value);
+
+
+            string hashString;
+            /*constant: 47ccecfc14f9ed9eff5de591b8614077*/
+            var people = new People();
+            hashString = people.HashString();
+            /*constant: 3953fbec5b81ccca72c98655c0c4b069*/
+            people = new People()
+            {
+                Id = 1,
+                Name = "Dennis Ritchie",
+                IsActive = false,
+                CreatedDateTime = new DateTime(1941, 9, 9)
+            };
+            hashString = people.HashString();
+
+
+
+
+            /*--------------------------------- Not good for comparing -----------------------------*/
+            var hashHelper = new HashHelper();
+            /*throws error as [Serializable] not been used*/
+            //var peopleHashString = hashHelper.HashString(people);
+            /*constant: 9105d073ad276d742c56a049abd4ddef
+             * will change if we change 
+             *      1. class name
+             *      2. property name
+             *      3. property data type
+             *      4. add/remove new property
+             */
+            var peopleModelHashString = hashHelper.HashString(new PeopleModel()
+            {
+                Id = 1,
+                Name = "Anders Hejlsberg",
+                IsActive = true,
+                CreatedDateTime = new DateTime(1960, 12, 2)
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
